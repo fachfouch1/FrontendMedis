b58649c0adc052f16e539d148c0d2ea597c6f3b5
FadhelShel <fadhel.shel@pinely.eu>
2024-03-11 00:01:55 +0100

    feat: add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navigation/NavBar";
 import SearchPage from "./components/Search/SearchPage";
+import ProfilePage from "./components/Profile/Profile";
+import DataPage from "./components/Data/DataPage";
 
 function App() {
   return (
     <>
-      <SearchPage />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/data" element={<ProfilePage />} />
+        <Route path="/profile" element={<DataPage />} />
+      </Routes>
     </>
   );
 }
