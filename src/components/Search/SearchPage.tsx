4a187515f7d25eb942433c96d618abf0a51dbe74
FadhelShel <fadhel.shel@pinely.eu>
2024-04-20 17:29:57 +0100

    feat: add loading and error states

--- src/components/Search/SearchPage.tsx
+++ src/components/Search/SearchPage.tsx
@@ -1,40 +1,44 @@
 import { useState } from "react";
 import styles from "./search-page.module.css";
 import { getMolecule } from "./utils";
+import clsx from "clsx";
 
 const SearchPage = () => {
-  const [sliderValue, setSliderValue] = useState(1); // Default slider value
+  const [sliderValue, setSliderValue] = useState(1);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const getMoleculeHandler = async () => {
+    setLoading(true);
     if (sliderValue && searchTerm.length > 0) {
-      await getMolecule(8, sliderValue, searchTerm);
+      const response = await getMolecule(8, sliderValue, searchTerm);
+      if (response?.molecule_id) {
+        // to do
+      } else {
+        setError(response.message);
+      }
     }
+    setLoading(false);
   };
 
   return (
     <div className={styles.container}>
       <div style={{ marginBottom: "20px" }}>
-        <img
-          src="/medisSearchLogo.png"
-          alt="searchMedis logo"
-          className={styles.logo}
-          width={"350px"}
-        />
+        <img src="/medisSearchLogo.png" alt="searchMedis logo" className={styles.logo} width={"350px"} />
       </div>
       <div className={styles.search_layout}>
         <input
           type="text"
           placeholder="Search..."
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className={styles.search_bar}
+          onChange={(e) => !loading && setSearchTerm(e.target.value)}
+          className={clsx(styles.search_bar, loading && styles.search_bar_disabled)}
         />
         <div className={styles.slider_button_layout}>
           <div className={styles.slider_container}>
             <span style={{ alignSelf: "flexStart" }}>
-              Number of articles:{" "}
-              <span className={styles.slider_count}>{sliderValue}</span>
+              Number of articles: <span className={styles.slider_count}>{sliderValue}</span>
             </span>
             <input
               type="range"
@@ -46,9 +50,17 @@ const SearchPage = () => {
               onChange={(e) => setSliderValue(+e.target.value)}
             />
           </div>
-          <button className={styles.search_button} onClick={getMoleculeHandler}>
-            Search
-          </button>
+          <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "20px" }}>
+            {loading && <span className="loader" />}
+            {error && <span className={styles.error_message}>{error}</span>}
+            <button
+              disabled={loading}
+              className={clsx(styles.search_button, loading && styles.search_button_disabled)}
+              onClick={getMoleculeHandler}
+            >
+              Search
+            </button>
+          </div>
         </div>
       </div>
     </div>
