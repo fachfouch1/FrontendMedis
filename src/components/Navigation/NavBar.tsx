39cf509fb84043feb688e91994f46c43eb740dbb
FadhelShel <fadhel.shel@pinely.eu>
2024-04-30 23:40:37 +0100

    add logout

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -2,32 +2,32 @@ import { NavLink } from "react-router-dom";
 import styles from "./navbar.module.css";
 
 const NavBar = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+  };
+
   return (
-    <div className={styles.navBarStyle}>
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          styles.buttonStyle + " " + (isActive ? styles.buttonActive : "")
-        }
-      >
-        Search
-      </NavLink>
-      <NavLink
-        to="/data"
-        className={({ isActive }) =>
-          styles.buttonStyle + " " + (isActive ? styles.buttonActive : "")
-        }
-      >
-        Data
-      </NavLink>
-      <NavLink
-        to="/profile"
-        className={({ isActive }) =>
-          styles.buttonStyle + " " + (isActive ? styles.buttonActive : "")
-        }
-      >
-        Profile
-      </NavLink>
+    <div className={styles.navBar}>
+      <div className={styles.navBarStyle}>
+        <NavLink to="/" className={({ isActive }) => styles.buttonStyle + " " + (isActive ? styles.buttonActive : "")}>
+          Search
+        </NavLink>
+        <NavLink
+          to="/data"
+          className={({ isActive }) => styles.buttonStyle + " " + (isActive ? styles.buttonActive : "")}
+        >
+          Data
+        </NavLink>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) => styles.buttonStyle + " " + (isActive ? styles.buttonActive : "")}
+        >
+          Profile
+        </NavLink>
+      </div>
+      <button style={{ zIndex: 1000 }} onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
