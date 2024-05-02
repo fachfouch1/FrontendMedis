d0e82c51fb7aa1e0285819aa8599b4a51d3cbcc9
FadhelShel <fadhel.shel@pinely.eu>
2024-05-02 21:46:03 +0100

    change route

--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { ROLE } from "../../services/types";
 
 const UsersPage = () => {
   const navigate = useNavigate();
@@ -8,7 +9,7 @@ const UsersPage = () => {
     const userStored = localStorage.getItem("userData");
     if (userStored) {
       const userData = JSON.parse(userStored);
-      if(!userData.status && userData.role !== "Admin") {
+      if(!userData.status && userData.role !== ROLE.Admin) {
         navigate("/profile");
       }
     }
